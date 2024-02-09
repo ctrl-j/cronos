ea3268f83b6555786fcefc7fb527261d9cdf0fb6
Joshua Jackson <joshua.d.jackson45@gmail.com>
2024-02-09 11:09:27 -0500

    Updated boot.asm
    Prints alternating upper and lower case chars from alphabet

--- src/boot.asm
+++ src/boot.asm
@@ -2,14 +2,30 @@ mov ah, 0x0E
 mov al, 'A'
 int 0x10
 
+mov bl, 0
+
 loop:
     inc al
-    cmp al, 'Z' + 1
+    cmp al, 'z' + 1
     je exit
 
+    cmp bl, 0
+    je lower
+    jne upper
+
+upper:
+    sub al, 32
+    dec bl
+    jmp printchar
+
+lower:
+    add al, 32
+    inc bl
+    jmp printchar
+
+printchar:
     mov ah, 0x0E
     int 0x10
-
     jmp loop
 
 exit:
